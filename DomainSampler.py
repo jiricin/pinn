553e7bab374a0822034ffecc571c6a356292270b
Jiří Cingel <cin0033@vsb.cz>
2023-09-07 17:35:45 +0200

    coded horizontal line sampler, turn check

--- DomainSampler.py
+++ DomainSampler.py
@@ -1,3 +1,5 @@
+import math
+
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -12,20 +14,26 @@ class DomainSampler:
         self.point_count = len(self.points[1])
         self.min_y = np.min(self.points[1])
         self.max_y = np.max(self.points[1])
+        self.min_x = np.min(self.points[0])
+        self.max_x = np.max(self.points[0])
 
-    def sample_line_prod(self):
+        self.samples = [[], []]
+
+    def random_line_prod(self):
         # get a random y value for sample
         y = np.random.uniform(low=self.min_y, high=self.max_y)
-        while self.points[1][-1] == y:
-            np.roll(self.points, 1, axis=1)  # for algorithm purposes
+        idx_shift = 0
+        while self.points[1][idx_shift-1] == y:
+            idx_shift = idx_shift + 1  # for algorithm purposes
 
         # we need to check for intersections between line Y=y and domain boundary
         ordered_intersections = []
 
         # indices for iterating over points and checking, if there was an intersection between them
-        idx_prev = -1
-        idx = 0
-        while idx < self.point_count:
+        idx_prev = idx_shift - 1
+        idx = idx_shift
+        point_count_shifted = self.point_count + idx_shift
+        while idx < point_count_shifted:
             # checking differences between point y-values and line Y=y
             sgn_prev = self.points[1][idx_prev] - y
             sgn = self.points[1][idx] - y
@@ -73,22 +81,104 @@ class DomainSampler:
         # return sampled point
         return [x, y]
 
+    def sample_line_prod(self):
+        self.samples = [[], []]
+
+        # for a list of y-values
+        n_y = 50
+        for y in np.linspace(self.min_y, self.max_y, n_y):
+            idx_shift = 0
+            while self.points[1][idx_shift-1] == y:
+                idx_shift = idx_shift + 1  # for algorithm purposes
+
+            # we need to check for intersections between line Y=y and domain boundary
+            ordered_intersections = []
+
+            # indices for iterating over points and checking, if there was an intersection between them
+            idx_prev = idx_shift - 1
+            idx = idx_shift
+            point_count_shifted = self.point_count + idx_shift
+            while idx < point_count_shifted:
+                # checking differences between point y-values and line Y=y
+                sgn_prev = self.points[1][idx_prev] - y
+                sgn = self.points[1][idx] - y
+
+                if sgn_prev * sgn < 0:  # True = intersection somewhere between points
+                    # intersection point position calculation
+                    x_is = self.points[0][idx_prev] - sgn_prev * \
+                           (self.points[0][idx] - self.points[0][idx_prev]) / \
+                           (self.points[1][idx] - self.points[1][idx_prev])
+                    ordered_intersections.append([x_is, x_is, True])  # boolean: does crossing it change domain inclusion?
+
+                if sgn == 0:  # True = intersection at an edge point
+                    x1_is = self.points[0][idx]  # store the position of the first intersection
+                    while sgn == 0:  # get the next point, which does not share the same y-value
+                        idx += 1
+                        sgn = self.points[1][idx] - y
+                    x2_is = self.points[0][idx - 1]  # store the position of the last intersection
+                    ordered_intersections.append([x1_is, x2_is, np.sign(sgn_prev) - np.sign(sgn)])
+
+                idx_prev = idx
+                idx += 1
+
+            # sort the intersections by increasing x-values
+            ordered_intersections.sort(key=lambda seg: (seg[0] + seg[1]) / 2)
+
+            # create intervals from intersections
+            ordered_intervals = []
+            inclusion = False  # by going from left to right, inclusion changes
+            for iseg in ordered_intersections:
+                if iseg[2]:  # True = crossing changes domain inclusion
+                    inclusion = ~inclusion
+                    ordered_intervals.append(iseg[1] if inclusion else iseg[0])  # assign appropriate interval border
+                elif inclusion:  # True = no inclusion change; if we are in the domain, we want to exclude the boundary
+                    ordered_intervals.append(iseg[0])
+                    ordered_intervals.append(iseg[1])
+
+            # compute x-values for y-valued intervals in domain
+            n_x = 50
+            x = np.linspace(self.min_x, self.max_x, n_x)
+            # intervals of indexes for array x
+            ordered_intervals = (ordered_intervals - self.min_x) * (n_x - 1) / (self.max_x - self.min_x)
+            int_idx = 0
+            while int_idx < len(ordered_intervals):
+                idx1 = math.ceil(ordered_intervals[int_idx])
+                idx2 = math.floor(ordered_intervals[int_idx + 1])
+                idx_count = idx2 - idx1 + 1
+                self.samples[0].extend(x[idx1:(idx2+1)])
+                self.samples[1].extend(np.full(idx_count, y))
+                int_idx = int_idx + 2
+
+            # repeat for all y
+
+        return
+
+    def turn_check(self, x, y):
+        deg_sum = 0.0
+        for idx in range(self.point_count):
+            p1 = self.points[0][idx - 1] - x
+            p2 = self.points[1][idx - 1] - y
+            n1 = self.points[0][idx] - x
+            n2 = self.points[1][idx] - y
+            pn = p1*n1+p2*n2
+            pp = p1*p1+p2*p2
+            nn = n1*n1+n2*n2
+            deg_sum = deg_sum + np.sign(p2 * (n1 - p1 * pn / pp)) * math.acos(pn / (math.sqrt(pp) * math.sqrt(nn)))
+
+        return ~(deg_sum < 5.0)
+
+    def plot(self):
+        plt.plot(self.samples[0], self.samples[1], 'g+')
+
 
 if __name__ == '__main__':
-    # Pacman
+    # Pacman domain
     ds = DomainSampler([[0, -1, -2, -1, 0, 1, 1, 0, -1, -2, -1],
                         [0, 1, 1, 2, 2, 1, -1, -2, -2, -1, -1]])
     plt.plot(ds.points[0], ds.points[1], 'b')
     plt.plot([ds.points[0][-1], ds.points[0][0]], [ds.points[1][-1], ds.points[1][0]], 'b')
 
-    for i in range(500):
-        sample = ds.sample_line_prod()
-        plt.plot(sample[0], sample[1], 'r+')
+    ds.sample_line_prod()
+    ds.plot()
 
     plt.show()
-
-# to do
-# - if points are on a horizontal line, algorithm gets stuck on line 19 (possibly 42, too)
-# - sorting complexity on line 52 can be alleviated by bisection in-sorting in element adding phase
-#
-# - clockwise-turning-degree-sum-based check and sampling
